7784f9b1def109ebc90f758327c43e6671d2c582
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-02-11 22:06:56 +0100

    Semiautomatic update of documentation

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["kraxn_server"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"kraxn_server/struct.Error.html\" title=\"struct kraxn_server::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"kraxn_server/enum.ErrorKind.html\" title=\"enum kraxn_server::ErrorKind\">ErrorKind</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/error_chain/trait.ChainedError.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["kraxn_server"] = ["impl <a class=\"trait\" href=\"https://docs.rs/error-chain/0.11.0/error_chain/trait.ChainedError.html\" title=\"trait error_chain::ChainedError\">ChainedError</a> for <a class=\"struct\" href=\"kraxn_server/struct.Error.html\" title=\"struct kraxn_server::Error\">Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
