9234237206daae10372923721e24d9fdb95e1a5d
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-02-11 15:54:45 +0100

    basic structure

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
+// Copyright 2018 Stefan Kroboth
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+//! TODO Documentation
+
+#![recursion_limit = "1024"]
+#![cfg_attr(feature = "clippy", feature(plugin))]
+#![cfg_attr(feature = "clippy", plugin(clippy))]
+#![warn(missing_docs)]
+#[macro_use]
+extern crate error_chain;
+
+fn run() -> Result<(), Box<std::error::Error>> {
     println!("Hello, world!");
+    Ok(())
+}
+
+fn main() {
+    if let Err(ref e) = run() {
+        println!("error: {}", e);
+    }
 }
