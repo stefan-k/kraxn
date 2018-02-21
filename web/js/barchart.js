94342fecb8f3e258b62eb51f9618280ae1787ac0
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-02-21 14:51:10 +0100

    slightly more complex example

--- web/js/barchart.js
+++ web/js/barchart.js
@@ -1,19 +1,57 @@
 var data = [4, 8, 15, 16, 23, 42];
 
+var width = 420,
+  bar_height = 20;
+
 var x = d3.scale
   .linear()
   .domain([0, d3.max(data)])
-  .range([0, 420]);
+  .range([0, width]);
 
-d3
+var chart = d3
   .select(".chart")
-  .selectAll("div")
+  .attr("width", width)
+  .attr("height", bar_height * data.length);
+
+var bar = chart
+  .selectAll("g")
   .data(data)
   .enter()
-  .append("div")
-  .style("width", function(d) {
-    return x(d) + "px";
+  .append("g")
+  .attr("transform", function(d, i) {
+    return "translate(0," + i * bar_height + ")";
+  });
+
+bar
+  .append("rect")
+  .attr("width", x)
+  .attr("height", bar_height - 1);
+
+bar
+  .append("text")
+  .attr("x", function(d) {
+    return x(d) - 3;
   })
+  .attr("y", bar_height / 2)
+  .attr("dy", ".35em")
   .text(function(d) {
     return d;
   });
+
+// var x = d3.scale
+//   .linear()
+//   .domain([0, d3.max(data)])
+//   .range([0, 420]);
+//
+// d3
+//   .select(".chart")
+//   .selectAll("div")
+//   .data(data)
+//   .enter()
+//   .append("div")
+//   .style("width", function(d) {
+//     return x(d) + "px";
+//   })
+//   .text(function(d) {
+//     return d;
+//   });
