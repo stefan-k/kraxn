4a5a40552b834cb25f98c7c92f04c3ab111392c8
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-02-22 13:45:40 +0100

    vertical

--- web/js/barchart.js
+++ web/js/barchart.js
@@ -1,23 +1,31 @@
 // var data = [4, 8, 15, 16, 23, 42];
 
-var width = 420,
-  bar_height = 20;
+var width = 900;
+var height = 400;
 
-var x = d3.scale.linear().range([0, width]);
+var x = d3.scale.ordinal().rangeRoundBands([0, width], 0.1);
 
-var chart = d3.select(".chart").attr("width", width);
+var y = d3.scale.linear().range([height, 0]);
 
-// 1. code here runs first, before the download starts
+var chart = d3
+  .select(".chart")
+  .attr("width", width)
+  .attr("height", height);
+
 d3.tsv("js/data.tsv", type, function(error, data) {
-  // 3. Code here runs last, after the download finishes.
-  x.domain([
+  x.domain(
+    data.map(function(d) {
+      return d.name;
+    })
+  );
+  y.domain([
     0,
     d3.max(data, function(d) {
       return d.value;
     })
   ]);
 
-  chart.attr("height", bar_height * data.length);
+  var bar_width = width / data.length;
 
   var bar = chart
     .selectAll("g")
@@ -25,23 +33,26 @@ d3.tsv("js/data.tsv", type, function(error, data) {
     .enter()
     .append("g")
     .attr("transform", function(d, i) {
-      return "translate(0," + i * bar_height + ")";
+      return "translate(" + x(d.name) + ",0)";
     });
 
   bar
     .append("rect")
-    .attr("width", function(d) {
-      return x(d.value);
+    .attr("y", function(d) {
+      return y(d.value) + 3;
     })
-    .attr("height", bar_height - 1);
+    .attr("height", function(d) {
+      return height - y(d.value);
+    })
+    .attr("width", x.rangeBand());
 
   bar
     .append("text")
-    .attr("x", function(d) {
-      return x(d.value) - 3;
+    .attr("x", x.rangeBand() / 2)
+    .attr("y", function(d) {
+      return y(d.value) + 3;
     })
-    .attr("y", bar_height / 2)
-    .attr("dy", ".35em")
+    .attr("dy", ".75em")
     .text(function(d) {
       return d.value;
     });
@@ -51,6 +62,58 @@ function type(d) {
   d.value = +d.value; // coerce to number
   return d;
 }
+
+// var width = 420,
+//   bar_height = 20;
+//
+// var x = d3.scale.linear().range([0, width]);
+//
+// var chart = d3.select(".chart").attr("width", width);
+//
+// // 1. code here runs first, before the download starts
+// d3.tsv("js/data.tsv", type, function(error, data) {
+//   // 3. Code here runs last, after the download finishes.
+//   x.domain([
+//     0,
+//     d3.max(data, function(d) {
+//       return d.value;
+//     })
+//   ]);
+//
+//   chart.attr("height", bar_height * data.length);
+//
+//   var bar = chart
+//     .selectAll("g")
+//     .data(data)
+//     .enter()
+//     .append("g")
+//     .attr("transform", function(d, i) {
+//       return "translate(0," + i * bar_height + ")";
+//     });
+//
+//   bar
+//     .append("rect")
+//     .attr("width", function(d) {
+//       return x(d.value);
+//     })
+//     .attr("height", bar_height - 1);
+//
+//   bar
+//     .append("text")
+//     .attr("x", function(d) {
+//       return x(d.value) - 3;
+//     })
+//     .attr("y", bar_height / 2)
+//     .attr("dy", ".35em")
+//     .text(function(d) {
+//       return d.value;
+//     });
+// });
+//
+// function type(d) {
+//   d.value = +d.value; // coerce to number
+//   return d;
+// }
 // 2. Code here runs second, while the file is downloading
 
 // var x = d3.scale
