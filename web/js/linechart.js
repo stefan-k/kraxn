4fc77d51e142fc978f22326b4757860f032084a4
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-02-24 17:32:23 +0100

    automatic update

--- web/js/linechart.js
+++ web/js/linechart.js
@@ -35,77 +35,52 @@ var line = d3.svg
 //   .orient("left")
 //   .ticks(10, "%");
 
+setInterval(plot, 1000);
+
 // d3.tsv("js/frequency.tsv", type, function(error, data) {
-d3.csv("data/1", type, function(error, data) {
-  if (error) throw error;
+function plot() {
+  d3.csv("data/1", type, function(error, data) {
+    if (error) throw error;
 
-  x.domain(
-    data.map(function(d) {
-      return d.x;
-    })
-  );
+    x.domain(
+      data.map(function(d) {
+        return d.x;
+      })
+    );
 
-  y.domain([
-    0,
-    d3.max(data, function(d) {
-      return d.y;
-    })
-  ]);
+    y.domain([
+      0,
+      d3.max(data, function(d) {
+        return d.y;
+      })
+    ]);
 
-  g
-    .append("g")
-    .attr("transform", "translate(0," + height + ")")
-    // .call(d3.axisBottom(x))
-    .select(".domain")
-    .remove();
+    g
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      // .call(d3.axisBottom(x))
+      .select(".domain")
+      .remove();
 
-  g
-    .append("path")
-    .datum(data)
-    .attr("fill", "black")
-    .attr("stroke", "steelblue")
-    .attr("stroke-linejoin", "round")
-    .attr("stroke-linecap", "round")
-    .attr("stroke-width", 1.5)
-    .attr("d", line);
-
-  // chart
-  //   .append("g")
-  //   .attr("class", "x axis")
-  //   .attr("transform", "translate(0," + height + ")")
-  //   .call(x_axis);
-  //
-  // chart
-  //   .append("g")
-  //   .attr("class", "y axis")
-  //   .call(y_axis)
-  //   .append("text")
-  //   .attr("transform", "rotate(-90)")
-  //   .attr("y", 6)
-  //   .attr("dy", ".71em")
-  //   .style("text-anchor", "end")
-  //   .text("Frequency");
-  //
-  // chart
-  //   .selectAll(".bar")
-  //   .data(data)
-  //   .enter()
-  //   .append("rect")
-  //   .attr("class", "bar")
-  //   .attr("x", function(d) {
-  //     return x(d.letter);
-  //   })
-  //   .attr("y", function(d) {
-  //     return y(d.frequency);
-  //   })
-  //   .attr("height", function(d) {
-  //     return height - y(d.frequency);
-  //   })
-  //   .attr("width", x.rangeBand());
-});
+    g
+      .append("path")
+      .datum(data)
+      .attr("fill", "black")
+      .attr("stroke", "steelblue")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+    console.log(data);
+  });
+}
 
 function type(d) {
   d.x = +d.x; // coerce to number
   d.y = +d.y; // coerce to number
   return d;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
