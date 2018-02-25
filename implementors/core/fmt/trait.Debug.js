62f6fb2c5d65e67ed29bcaad9ea23f3e8ae8ef03
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-02-25 17:59:16 +0100

    Semiautomatic update of documentation

--- implementors/core/fmt/trait.Debug.js
+++ implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["kraxn_server"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"kraxn_server/struct.DataP.html\" title=\"struct kraxn_server::DataP\">DataP</a>",];
+implementors["kraxn_server"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"kraxn_server/struct.DataP.html\" title=\"struct kraxn_server::DataP\">DataP</a>",synthetic:false,types:["kraxn_server::DataP"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
